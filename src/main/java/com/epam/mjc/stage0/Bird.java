a61431dd5ab63d2b6d14e481af3af4f7e8ee40bc
Quartzuwka <kvashnin.semion@yandex.ru>
2025-11-11 22:22:13 +0300

    done

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    Animal animal = new Animal("1",2,true);
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
